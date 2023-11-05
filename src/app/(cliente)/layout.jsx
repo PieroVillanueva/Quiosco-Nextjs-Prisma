b833f0205c7b127a45c85432baa462da11646d36
PieroVillanueva <pierofabrizzio9@hotmail.com>
2023-11-05 15:28:39 -0500

    optimizacion Layouts

--- src/app/(cliente)/layout.jsx
+++ src/app/(cliente)/layout.jsx
@@ -1,40 +1,27 @@
-import { Inter } from "next/font/google";
-
 import Sidebar from "../../components/Sidebar";
 import Pasos from "@/components/Pasos";
 
 import { QuioscoProvider } from "../../context/QuioscoProvider";
 
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import "../globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
-
 export const metadata = {
-  title: "Café",
-  description: "Quiosco Cafetería",
+  title: "Café - Menú",
+  description: "Menú del Quiosco Cafetería",
 };
 
-export default function RootLayout({ children }) {
+export default function LayoutCliente({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <QuioscoProvider>
-          <div className="md:flex">
-            <aside className="md:w-4/12 xl:w-1/4 2xl:w-1/5 ">
-              <Sidebar />
-            </aside>
-            <main className="md:w-8/12 xl:w-3/4 2xl:w-4/5 h-screen overflow-y-scroll">
-              <div className="p-10">
-                <Pasos />
-                {children}
-              </div>
-            </main>
+    <QuioscoProvider>
+      <div className="md:flex">
+        <aside className="md:w-4/12 xl:w-1/4 2xl:w-1/5 ">
+          <Sidebar />
+        </aside>
+        <main className="md:w-8/12 xl:w-3/4 2xl:w-4/5 h-screen overflow-y-scroll">
+          <div className="p-10">
+            <Pasos />
+            {children}
           </div>
-          <ToastContainer />
-        </QuioscoProvider>
-      </body>
-    </html>
+        </main>
+      </div>
+    </QuioscoProvider>
   );
 }
---
+++ src/app/layout.jsx
@@ -0,0 +1,22 @@
+import { Inter } from "next/font/google";
+
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Café",
+  description: "Quiosco Cafetería",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="es">
+      <body className={inter.className}>
+        {children} <ToastContainer />
+      </body>
+    </html>
+  );
+}
