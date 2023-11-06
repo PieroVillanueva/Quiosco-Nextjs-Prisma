e36ef4eb39761f40da7bc89f05d1c16bf84f1482
PieroVillanueva <pierofabrizzio9@hotmail.com>
2023-11-05 21:17:02 -0500

    Agregado Icon

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,4 @@
 import { Inter } from "next/font/google";
-
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./globals.css";
@@ -9,6 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Café",
   description: "Quiosco Cafetería",
+  icons: "/favicon.png",
 };
 
 export default function RootLayout({ children }) {
